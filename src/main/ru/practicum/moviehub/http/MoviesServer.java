dfb0eb24e6767ae1124e2c0f2c30fd143eded8cc
Павел <pashalperm@mail.ru>
2026-02-24 04:52:25 +0500

    1

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -1,5 +1,39 @@
 package ru.practicum.moviehub.http;
 
+import com.sun.net.httpserver.HttpServer;
+import ru.practicum.moviehub.store.MoviesStore;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class MoviesServer {
+
+    private final HttpServer server;
+    private final MoviesStore store;
+
+    public MoviesServer(MoviesStore store, int port) {
+        this.store = store;
+        try {
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось создать HTTP сервер", e);
+        }
+
+        server.createContext("/movies", new MoviesHandler(this.store));
+    }
+
+    public MoviesStore getStore() {
+        return store;
+    }
+
+    public void start() {
+        server.start();
+        System.out.println("Сервер запущен");
+    }
+
+    public void stop() {
+        server.stop(0);
+        System.out.println("Сервер остановлен");
+    }
 
 }
